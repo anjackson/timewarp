bd480cf9cd1016fc7a502430d0905d2ff6982e1a
Andrew Jackson <Andrew.Jackson@bl.uk>
2014-10-15 10:59:56 +0100

    Added basic timewarp web proxy.

---
+++ timewarp_proxy.py
@@ -0,0 +1,11 @@
+import time
+from libmproxy.script import concurrent
+
+#@concurrent
+def request(context, flow):
+    print "handle request: %s - %s" % (flow.request.host, flow.request.path)
+    # http://web.archive.org/web/19981212031357
+    if not "id_/http" in flow.request.path:
+	    flow.request.path = "/web/19981201052808id_/http://%s%s" % (flow.request.host, flow.request.path)
+    flow.request.host = "web.archive.org"
+    flow.request.headers["Host"] = ["web.archive.org"]
