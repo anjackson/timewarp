2d4409e99f3ad7b530669ec1815d620139fb06f5
Andrew Jackson <Andrew.Jackson@bl.uk>
2014-10-15 11:12:16 +0100

    Added getting-started info and reduced the logging.

--- README.md
+++ README.md
@@ -2,3 +2,21 @@ timewarp
 ========
 
 Making it easier to browse the past.
+
+Quick Start
+-----------
+
+Step 1: Install [MITM Proxy]:
+
+    $ pip install mitmproxy
+
+Step 2: Run the proxy:
+
+    $ mitmdump -s timewarp_proxy.py
+
+Step 3: Set your web browser to use ```servername:8080``` as your web proxy.
+
+Issues
+------
+
+- Hard-coded 1998 datestamp.
--- timewarp_proxy.py
+++ timewarp_proxy.py
@@ -1,9 +1,9 @@
 import time
 from libmproxy.script import concurrent
 
-#@concurrent
+@concurrent
 def request(context, flow):
-    print "handle request: %s - %s" % (flow.request.host, flow.request.path)
+    #print "handle request: %s - %s" % (flow.request.host, flow.request.path)
     # http://web.archive.org/web/19981212031357
     if not "id_/http" in flow.request.path:
 	    flow.request.path = "/web/19981201052808id_/http://%s%s" % (flow.request.host, flow.request.path)
