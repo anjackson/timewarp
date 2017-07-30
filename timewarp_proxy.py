44d8242089eabde06a147f8257fac84dcf2a04ac
Andrew Jackson <Andrew.Jackson@bl.uk>
2017-07-30 20:48:17 +0100

    Added standard Python files.

--- timewarp_proxy.py
+++ timewarp_proxy.py
@@ -1,4 +1,6 @@
 import time
+import datetime
+from memento_client import MementoClient
 
 from mitmproxy.script import concurrent
 
@@ -11,11 +13,11 @@ def request(flow):
     print("handle request: %s%s" % (flow.request.host, flow.request.path))
     # http://web.archive.org/web/19981212031357
     if not "id_/http" in flow.request.path:
-	    flow.request.path = "/web/19981201052808id_/http://%s%s" % (flow.request.host, flow.request.path)
-    flow.request.scheme = "https"
-    flow.request.host = "web.archive.org"
-    flow.request.port = 443
-    flow.request.headers["Host"] = "web.archive.org"
+        dt = datetime.datetime(2010, 4, 24, 19, 0)
+        uri = flow.request.url
+        mc = MementoClient()
+        memento_uri = mc.get_memento_info(uri, dt).get("mementos").get("closest").get("uri")[0]
+        flow.request.url = memento_uri
 
 @concurrent
 def response(flow):
